c87189c3379b3d38254ec80a6ab8e4d76904f05a
mirunaen <mirunaen@gmail.com>
2020-09-08 20:05:16 +0200

    minor changes

--- static/home.js
+++ static/home.js
@@ -2,10 +2,11 @@
 
 function ageInDays() {
   var birthYear = prompt("Enter birthYear");
-  var result = (2020 - birthYear) * 365;
+  var ageInDayss = (2020 - birthYear) * 365;
   var h1 = document.createElement("h1");
-  var textAnswer = document.createTextNode("You are " + result + "days");
-  h1.setAttribute("id", "result");
+  var textAnswer = document.createTextNode("You are " + ageInDayss + " days");
+  h1.setAttribute("id", "ageInDayss");
+  h1.appendChild(textAnswer);
   document.getElementById("flex-box-result").appendChild(h1);
   console.log(result);
 }
