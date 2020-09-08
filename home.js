627ad5c298dc06126b4daace8b2c2629af3078e1
mirunaen <mirunaen@gmail.com>
2020-09-08 18:18:00 +0200

    changes

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
-var age = prompt("Enter age");
+function message(age) {
+  var age = prompt("Enter age");
+  document.getElementById("someText").innerHTML = age;
+  if ((age) => 50) {
+    return "Hey there Oldie ;( ";
+  } else {
+    return "Hey there Youngster! ";
+  }
+}
 
-document.getElementById(paraf).innerHTML =  "Hey,you" + ;
+message();
