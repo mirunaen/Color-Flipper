ded9dabc03c5b29629c43c1d266a1e904deada30
mirunaen <mirunaen@gmail.com>
2020-09-08 19:00:43 +0200

    minor changes

--- static/home.js
+++ static/home.js
@@ -1,1 +1,8 @@
+//Challenge 1: Your age in days
 
+function ageInDays() {
+  var birthYear = prompt("Enter birthYear");
+  var dt = new Date();
+  var year = document.write(dt.getFullYear());
+  return (birthYear - year) * 360;
+}
